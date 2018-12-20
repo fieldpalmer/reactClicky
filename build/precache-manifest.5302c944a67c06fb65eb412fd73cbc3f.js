faaf08c4416477615d294e6cb4a33ece9cc45b86
fieldpalmer <gfieldpalmer@gmail.com>
2018-12-19 23:18:16 -0500

    changed package.json name

---
+++ build/precache-manifest.5302c944a67c06fb65eb412fd73cbc3f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "579f9036ff38ee3f57024be6b9f74871",
+    "url": "/reactClicky/static/media/comet.579f9036.png"
+  },
+  {
+    "revision": "dbd627e5328baa24ab3e",
+    "url": "/reactClicky/static/css/main.064caae3.chunk.css"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactClicky/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "eee416afeb9cbe2e594e",
+    "url": "/reactClicky/static/js/runtime~main.eee416af.js"
+  },
+  {
+    "revision": "3e5d37ddd365d07f0e4fa81ba93e5100",
+    "url": "/reactClicky/static/media/Screen Shot 2018-12-11 at 8.46.18 PM.3e5d37dd.png"
+  },
+  {
+    "revision": "b96c84825d347c20586c0c10676f654a",
+    "url": "/reactClicky/static/media/Screen Shot 2018-12-11 at 8.46.27 PM.b96c8482.png"
+  },
+  {
+    "revision": "52a3f2c8a2cd6ac6c3644b06ec295e80",
+    "url": "/reactClicky/static/media/blitzen.52a3f2c8.png"
+  },
+  {
+    "revision": "c74ba0185b34c5c42cb447515ef9d914",
+    "url": "/reactClicky/static/media/christmas-2948851.c74ba018.png"
+  },
+  {
+    "revision": "dbd627e5328baa24ab3e",
+    "url": "/reactClicky/static/js/main.dbd627e5.chunk.js"
+  },
+  {
+    "revision": "33a5c42e0fb58885aa17a2baa01eca75",
+    "url": "/reactClicky/static/media/cupid.33a5c42e.png"
+  },
+  {
+    "revision": "835fd2af6e1f369635dd306c562d2778",
+    "url": "/reactClicky/static/media/dancer.835fd2af.png"
+  },
+  {
+    "revision": "0892151cf52f681b50791d09696816ce",
+    "url": "/reactClicky/static/media/dasher.0892151c.png"
+  },
+  {
+    "revision": "65b132b5bd06550e6afa447868535745",
+    "url": "/reactClicky/static/media/donner.65b132b5.png"
+  },
+  {
+    "revision": "4b124effe0757d90c2feb1ff333f01f9",
+    "url": "/reactClicky/static/media/prancer.4b124eff.png"
+  },
+  {
+    "revision": "0b47d8e8ea92669964f319e55c9a3cd2",
+    "url": "/reactClicky/static/media/rudolph.0b47d8e8.png"
+  },
+  {
+    "revision": "71b9b439ef25579ec8c88cb6015a7fb3",
+    "url": "/reactClicky/static/media/vixen.71b9b439.png"
+  },
+  {
+    "revision": "b9c38d9594346a27044b3cdc31e10869",
+    "url": "/reactClicky/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactClicky/precache-manifest.5302c944a67c06fb65eb412fd73cbc3f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactClicky/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
